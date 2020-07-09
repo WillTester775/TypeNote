b1d7667ed6d8d28b8faf7100c8d81539cd1c82b3
brenh <brenh@DESKTOP-P16JN6B>
2020-07-09 11:51:31 +0800

    Add project files.

---
+++ UftDeveloperTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HP.LFT.SDK;
+using HP.LFT.Verifications;
+using HP.LFT.SDK.StdWin;
+
+namespace TypeNote
+{
+    [TestClass]
+    public class UftDeveloperTest : UnitTestClassBase<UftDeveloperTest>
+    {
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            GlobalSetup(context);
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var notepadWindow = Desktop.Describe<IWindow>(new WindowDescription
+            {
+                IsChildWindow = false,
+                IsOwnedWindow = false,
+                WindowClassRegExp = @"Notepad",
+                WindowTitleRegExp = @"Notepad"
+            });
+            var editEditor = notepadWindow.Describe<IEditor>(new EditorDescription
+            {
+                NativeClass = @"Edit"
+            });
+            
+            editEditor.SetCursorPosition(0, 0);
+            editEditor.SendKeys("hello world");
+
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            GlobalTearDown();
+        }
+    }
+}
